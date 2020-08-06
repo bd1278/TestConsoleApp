e2550f4a0fcf4600677300cbe890a9c972f63f6a
Bharadwaj Dhothre <bharadwajdhothre@outlook.com>
2020-08-06 16:28:17 -0400

    update 1

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -6,7 +6,8 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is update 1");
+
         }
     }
 }
